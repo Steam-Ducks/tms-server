697d954364426140bc47088244d6c6d3993b517b
Carlos Daniel <carlosdanielalves2006@gmail.com>
2025-09-05 20:47:11 -0300

    feat: added docker build

---
+++ spring-app/src/test/java/org/example/tmsserver/TmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tmsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
