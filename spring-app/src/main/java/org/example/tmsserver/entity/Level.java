d3abd72260f01b07a55f40ebe64f36e29a42eb06
Rafaella Cruz <rafaella.leticiaa@gmail.com>
2025-09-19 21:33:13 -0300

    fix (TMS-12): fixing value column type

--- spring-app/src/main/java/org/example/tmsserver/entity/Level.java
+++ spring-app/src/main/java/org/example/tmsserver/entity/Level.java
@@ -12,7 +12,7 @@ public class Level {
     private Long idLevel;
 
     @Column(name = "value")
-    private Double value;
+    private float value;
 
     @Column(name = "time")
     private LocalDateTime time;
@@ -23,7 +23,7 @@ public class Level {
 
     public Level() {}
 
-    public Level(Long idLevel, Double value, LocalDateTime time, Region region) {
+    public Level(Long idLevel, float value, LocalDateTime time, Region region) {
         this.idLevel = idLevel;
         this.value = value;
         this.time = time;
@@ -38,11 +38,11 @@ public class Level {
         this.idLevel = idLevel;
     }
 
-    public Double getValue() {
+    public float getValue() {
         return value;
     }
 
-    public void setValue(Double value) {
+    public void setValue(float value) {
         this.value = value;
     }
 
