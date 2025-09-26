5f570854ae802ad655572d0b4a7b49ffce1efaae
Samuelprado99 <samuelprado.99@hotmail.com>
2025-09-26 12:31:56 -0300

    feat (TMS-37): add method to fetch last RegionIndicator by region and indicator

--- spring-app/src/main/java/org/example/tmsserver/repository/RegionIndicatorRepository.java
+++ spring-app/src/main/java/org/example/tmsserver/repository/RegionIndicatorRepository.java
@@ -1,9 +1,14 @@
 package org.example.tmsserver.repository;
 
+import java.util.Optional;// S
+import org.example.tmsserver.entity.Region;//S  
+import org.example.tmsserver.entity.Indicator;//S
 import org.example.tmsserver.entity.RegionIndicator;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegionIndicatorRepository extends JpaRepository<RegionIndicator, Long> {
+    Optional<RegionIndicator> findTopByRegionAndIndicatorOrderByTimeDesc(Region region, Indicator indicator); //S
+
 }
