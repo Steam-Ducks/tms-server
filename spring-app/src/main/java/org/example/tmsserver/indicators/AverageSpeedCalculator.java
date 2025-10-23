4018356c89546019b91ffbe7b9b3f6ef6c0aeeb8
Lucas Acosta <lucas.heck@live.com>
2025-10-22 21:03:57 -0300

    refactor (TMS-55): set closer brackets for returning the average speed level

--- spring-app/src/main/java/org/example/tmsserver/indicators/AverageSpeedCalculator.java
+++ spring-app/src/main/java/org/example/tmsserver/indicators/AverageSpeedCalculator.java
@@ -9,10 +9,10 @@ public class AverageSpeedCalculator implements IndicatorCalculator {
 
     @Override
     public int mapValueToLevel(int value) {
-        if (value >= 77) return 1;
-        if (value >= 70) return 2;
-        if (value >= 60) return 3;
-        if (value >= 55) return 4;
+        if (value >= 75) return 1;
+        if (value >= 73) return 2;
+        if (value >= 70) return 3;
+        if (value >= 68) return 4;
         return 5;
     }
 }
